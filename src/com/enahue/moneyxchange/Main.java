9a1550f17aebc380f5fed6fbd09133efaa245722
Erwin Nahue <erwinnahue@gmail.com>
2024-05-20 13:17:41 -0500

    Added getAvailableCurrencies method to ExchangeRateAPI

--- src/com/enahue/moneyxchange/Main.java
+++ src/com/enahue/moneyxchange/Main.java
@@ -1,6 +1,8 @@
 package com.enahue.moneyxchange;
 
 import java.util.Scanner;
+import java.util.List;
+
 import com.enahue.moneyxchange.service.Conversor;
 import com.enahue.moneyxchange.service.ExchangeRateAPI;
 import com.enahue.moneyxchange.model.Response;
@@ -10,21 +12,50 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Conversor conversor = new Conversor();
         ExchangeRateAPI api = new ExchangeRateAPI();
+        int option;
+        do {
+            System.out.println("""
+                                        
+                    Selecciona una opción:
+                    1.- Ver divisas disponibles
+                    2.- Convertir divisa
+                    3.- salir""");
+            option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    List<String> currencies = api.getAvailableCurrencies();
+                    System.out.println("Divisas disponibles:");
+                    for (String currency : currencies) {
+                        System.out.println(currency);
+                    }
 
-        System.out.println("Enter the currency code you want to convert from (e.g., USD):");
-        String fromCurrency = scanner.nextLine();
+                    break;
+                case 2:
+                    System.out.println("Ingresa el código de la moneda desde la que quieres convertir (por ejemplo, USD):");
+                    scanner.nextLine();
+                    String fromCurrency = scanner.nextLine().toUpperCase();
 
-        System.out.println("Enter the currency code you want to convert to (e.g., EUR):");
-        String toCurrency = scanner.nextLine();
+                    System.out.println("Ingresa el código de la moneda a la que quieres convertir (por ejemplo, EUR):");
+                    String toCurrency = scanner.nextLine().toUpperCase();
 
-        System.out.println("Ingresa el monto a convertir:");
-        double amount = scanner.nextDouble();
+                    System.out.println("Ingresa el monto a convertir:");
+                    double amount = scanner.nextDouble();
 
-        Response response = api.getLatestRates(fromCurrency);
-        double exchangeRate = response.getExchangeRate(toCurrency);
+                    Response response = api.getLatestRates(fromCurrency);
+                    double exchangeRate = response.getExchangeRate(toCurrency);
 
-        double convertedAmount = conversor.convert(amount, exchangeRate);
+                    double convertedAmount = conversor.convert(amount, exchangeRate);
 
-        System.out.println("Monto convertido: " + convertedAmount);
+                    System.out.println("Monto convertido: " + convertedAmount);
+                    break;
+                case 3:
+                    System.out.println("Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, intenta de nuevo.");
+
+            }
+        } while (option != 3);
+
     }
 }
--- src/com/enahue/moneyxchange/service/ExchangeRateAPI.java
+++ src/com/enahue/moneyxchange/service/ExchangeRateAPI.java
@@ -1,11 +1,14 @@
 package com.enahue.moneyxchange.service;
+
 import com.enahue.moneyxchange.model.Response; // Add this import
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
+import java.util.List;
 public class ExchangeRateAPI {
     private static final String API_KEY = "9b7dcef9058c31be20dbcaf4";
 
@@ -21,4 +24,20 @@ public class ExchangeRateAPI {
             throw new RuntimeException("Error while calling API", e);
         }
     }
+
+    public List<String> getAvailableCurrencies() {
+        URI apiUri = URI.create("https://v6.exchangerate-api.com/v6/" + API_KEY + "/codes");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(apiUri).build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), new TypeToken<List<String>>() {
+            }.getType());
+        } catch (Exception e) {
+            throw new RuntimeException("Error while calling API", e);
+        }
+    }
+
+
 }
